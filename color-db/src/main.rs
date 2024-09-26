1dadf6282499a8bf6e48adc85b6ea44ce01f5240
The0x539 <the0x539@gmail.com>
2024-09-26 14:18:27 -0500

    generate ldconfig

--- color-db/src/main.rs
+++ color-db/src/main.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashMap, path::Path};
 
-use sqlx::{sqlite::SqliteConnectOptions, Acquire, SqlitePool};
+use sqlx::{sqlite::SqliteConnectOptions, Acquire, SqliteConnection, SqlitePool};
 
 pub mod ldraw;
 
@@ -8,10 +8,12 @@ pub mod studio;
 
 pub mod eyesight;
 
+type Result<T, E = Box<dyn std::error::Error>> = std::result::Result<T, E>;
+
 #[tokio::main(flavor = "current_thread")]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+async fn main() -> Result<()> {
     let path = Path::new(env!("CARGO_MANIFEST_DIR")).join("colors.sqlite");
-    let _ = std::fs::remove_file(&path);
+    // let _ = std::fs::remove_file(&path);
 
     let options = SqliteConnectOptions::new()
         .filename(&path)
@@ -21,8 +23,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut pool_conn = pool.acquire().await?;
     let conn = pool_conn.acquire().await?;
 
-    sqlx::migrate!("./migrations").run(&mut *conn).await?;
+    // sqlx::migrate!("./migrations").run(&mut *conn).await?;
+    // load(&mut *conn).await?;
 
+    ldconfig(&mut *conn).await?;
+
+    Ok(())
+}
+
+async fn load(conn: &mut SqliteConnection) -> Result<()> {
     let ldraw_paths = [
         "/mnt/c/program files/studio 2.0/ldraw/ldconfig.ldr",
         "/mnt/c/users/andrew/documents/lego/rendering/ldraw/ldconfig.ldr",
@@ -55,6 +64,10 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         eyesight::insert_file(&definitions, name, &mut *conn).await?;
     }
 
+    Ok(())
+}
+
+pub async fn python_dict(conn: &mut SqliteConnection) -> Result<()> {
     let rows = sqlx::query!("SELECT * FROM eyesight_color")
         .fetch_all(&mut *conn)
         .await?;
@@ -80,14 +93,8 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             _ => continue,
         };
 
-        let mut name = row
-            .studio_name
-            .replace("-", "_")
-            .replace(" ", "_")
-            .to_uppercase();
-
+        let mut name = row.studio_name.replace(['-', ' '], "_").to_uppercase();
         name.insert_str(0, prefix);
-
         name = name.replace("TRANS-TRANS_", "TRANS-");
 
         let Some(rgb) = map.get(&name) else {
@@ -104,6 +111,74 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+pub async fn ldconfig(conn: &mut SqliteConnection) -> Result<()> {
+    let ldraw_colors = sqlx::query!(
+        "
+        SELECT DISTINCT
+            ldraw.name,
+            ldraw.code,
+            ldraw.edge,
+            ldraw.alpha,
+            studio.studio_name,
+            studio.category_name
+        FROM ldraw_color ldraw
+            INNER JOIN studio_color studio ON studio.ldraw_code = ldraw.code
+        "
+    )
+    .fetch_all(&mut *conn)
+    .await?;
+
+    let eyesight_colors =
+        sqlx::query!("SELECT DISTINCT name, red, green, blue FROM eyesight_color")
+            .fetch_all(&mut *conn)
+            .await?
+            .into_iter()
+            .map(|record| {
+                let [r, g, b] = [record.red, record.green, record.blue].map(|n| (n * 255.0) as u8);
+                let color = format!("#{r:02X}{g:02X}{b:02X}");
+                (record.name, color)
+            })
+            .collect::<HashMap<_, _>>();
+
+    for c in &ldraw_colors {
+        let Some(category) = c.category_name.as_deref() else {
+            continue;
+        };
+        let prefix = match category {
+            "Solid Colors" => "SOLID-",
+            "Transparent Colors" => "TRANS-",
+            _ => continue,
+        };
+
+        let Some(studio_name) = c.studio_name.as_deref() else {
+            continue;
+        };
+
+        let mut eyesight_name = studio_name
+            .trim_start_matches("Trans-")
+            .replace(['-', ' '], "_")
+            .to_uppercase();
+
+        eyesight_name.insert_str(0, prefix);
+
+        let Some(eyesight_color) = eyesight_colors.get(&eyesight_name) else {
+            continue;
+        };
+
+        let edge = format!("#{:06X}", c.edge);
+
+        print!(
+            "0 !COLOUR {} CODE {} VALUE {} EDGE {}",
+            c.name, c.code, eyesight_color, edge
+        );
+        if let Some(alpha) = c.alpha {
+            print!(" ALPHA {alpha}");
+        }
+        println!();
+    }
+
+    Ok(())
+}
 fn parse_hex(s: &str) -> Option<u32> {
     let s = s.strip_prefix("#")?;
     if s.len() != 6 {
